525086ef7f5bba693ff9f8c7ebe89e7972448859
Seidmani64 <A01028650@itesm.mx>
2020-09-24 12:08:21 -0500

    Added tests to answer questions

--- reto.cpp
+++ reto.cpp
@@ -24,8 +24,12 @@ bool compareIP(T &a, T &b)
 }
 
 template <typename T>
-bool compareHostname(T &a, T &b)
+bool compareName(T &a, T &b)
 {
+    if(a == "-" && b != "-"){return true;}
+    else if(a != "-" && b == "-"){return false;}
+    else if(a == "-" && b == "-"){return false;}
+
     if(a<b)
     {
         return true;
@@ -33,23 +37,46 @@ bool compareHostname(T &a, T &b)
     return false;
 }
 
+//Main utilizado para hacer pruebas y encontrar las respuestas de las preguntas
 int main() 
 {
+    //Inicializacion
     ADT<string> test("equipo9.csv");
-    vector<string> hostnames;
 
+    cout<<"La cantidad de registros es de: "<<test.data.size()<<endl;
+
+
+    //Establecer y llenar vector con la columna de hostnames
+    vector<string> hostnames;
     for(size_t i = 0; i < test.data.size(); i++)
     {
         hostnames.push_back(test.data[i][4]);
     }
 
-    InsertionSort<string> hostSort;
-    hostSort.sort(hostnames,&compareHostname);
-
+    //Hacer Objetos de Sort y Search y organizar el vector de hostnames
+    InsertionSort<string> nameSort;
+    nameSort.sort(hostnames,&compareName);
     Search<string> find;
-    int elementIdx = find.orderedSeq1(hostnames,"betty.reto.com", &compareHostname); 
+
+    //Imprimir indice del termino buscado y si no es -1 imprimir el termino
+    int elementIdx = find.orderedSeq1(hostnames,"server.reto.com", &compareName); 
     cout<<elementIdx<<endl;
-    cout<<hostnames[elementIdx]<<endl;
+    if(elementIdx >= 0){cout<<hostnames[elementIdx]<<endl;}
+
+    //Repetir lo anterior para la columna de destination names
+    vector<string> destnames;
+    
+    for(size_t i = 0; i < test.data.size(); i++)
+    {
+        destnames.push_back(test.data[i][7]);
+    }
+
+    nameSort.sort(destnames, &compareName);
+    int elementIdx2 = find.orderedSeq1(destnames,"protonmail.com", &compareName); 
+    cout<<elementIdx2<<endl;
+    if(elementIdx2 >= 0){cout<<destnames[elementIdx2]<<endl;}
+
+
 
     return 0;
 }
