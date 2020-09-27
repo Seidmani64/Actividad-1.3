29f0231c7e71235694f059c3f18083ccdc2e6371
GianlucaBeltran <gianlucabeltran@gmail.com>
2020-09-26 22:58:03 -0500

    Changed ADT implementation

--- ADT.h
+++ ADT.h
@@ -2,46 +2,31 @@
 #define _READ_RECORD_FROM_FILE_H
 
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
 template <typename T>
-class ADT
+class Record
 {
-    private:
-        string file_name; // Nombre del csv.
-
-        void readData() // Funcion encargada de guardar info en vector.
-        {
-            ifstream file; // Crear  ifstream, biblioteca fstream.
-            file.open(file_name); // Abrir csv.
-
-            while(file.peek() != EOF) // Iterar hasta llegar al final del csv.
-            {
-                vector <string> fields(8); // Vector temporal para guardar todo un renglon.
-                getline(file, fields[0], ',');
-                getline(file, fields[1], ','); // getline(nombre del archivo, variable en la que se guarda el valor, limitante para guardar cada campo)
-                getline(file, fields[2], ',');
-                getline(file, fields[3], ',');
-                getline(file, fields[4], ',');
-                getline(file, fields[5], ',');
-                getline(file, fields[6], ',');
-                getline(file, fields[7], '\n');
-
-                data.push_back(fields); // Guardar renglon en vector principal
-            }
-        }
     public:
-        vector <vector<T>> data; // Vector donde se guardara toda la info del csv.
-        ADT(string _file_name) // Constructor
+        vector<T> row;
+
+        Record(vector<T> _row) // Constructor
         {
-            file_name = _file_name;
-            readData(); // Test para probar si funciona.
+            row = _row;
         }
-        ~ADT() {};
+        ~Record() {};
+
+        T getDate() {return row[0];}
+        T getTime() {return row[1];}
+        T getInerIp() {return row[2];}
+        T getInerPort() {return row[3];}
+        T getUser() {return row[4];}
+        T getOuterIp() {return row[5];}
+        T getOuterPort() {return row[6];}
+        T getWebsite() {return row[7];}
 };
 
 #endif
